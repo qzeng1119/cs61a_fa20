489946c527b59c3892b3c4cee386d763bd6777c1
qzeng1119 <bushiwo_845@sjtu.edu.cn>
2025-09-22 18:23:04 +0800

    upload hw07

---
+++ homework/hw07/hw07.scm
@@ -0,0 +1,41 @@
+(define (filter-lst fn lst)
+  'YOUR-CODE-HERE
+  (if (null? lst)
+      lst
+      (if (fn (car lst))
+          (cons (car lst) (filter-lst fn (cdr lst)))
+          (filter-lst fn (cdr lst)))))
+
+;;; Tests
+(define (even? x) (= (modulo x 2) 0))
+
+(filter-lst even? '(0 1 1 2 3 5 8))
+
+; expect (0 2 8)
+(define (interleave first second)
+    'YOUR-CODE-HERE
+    (cond ((null? first) second)
+          ((null? second) first)
+          (else (cons (car first) (cons (car second) (interleave (cdr first) (cdr second))))) 
+    ))
+
+(interleave (list 1 3 5) (list 2 4 6))
+
+; expect (1 2 3 4 5 6)
+(interleave (list 1 3 5) nil)
+
+; expect (1 3 5)
+(interleave (list 1 3 5) (list 2 4))
+
+; expect (1 2 3 4 5)
+(define (accumulate combiner start n term)
+  'YOUR-CODE-HERE
+  (if (= n 0)
+      start
+      (combiner (accumulate combiner start (- n 1) term) (term n))))
+
+(define (no-repeats lst) 
+    'YOUR-CODE-HERE
+    (if (null? lst)
+        lst
+        (cons (car lst) (filter-lst (lambda (x) (not (= x (car lst)))) (no-repeats (cdr lst))))))
